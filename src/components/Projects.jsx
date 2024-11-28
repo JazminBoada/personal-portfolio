aba171341dace026a2d49cef73e0d68f4ab7a359
Jazmin Boada <boadajazmin@gmail.com>
2024-11-28 18:21:00 -0300

    add new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -34,15 +34,15 @@ const Projects = () => {
 
             <ProjectsCards
               image={img3}
-              title="Todo List App"
+              title="Notes List App"
               buttons={[
                 { title: "HTML5", icon: <FaHtml5 /> },
-                { title: "CSS", icon: <FaCss3Alt /> },
-                { title: "JavaScript", icon: <IoLogoJavascript /> },
+                { title: "TailwindCSS", icon: <RiTailwindCssFill /> },
+                { title: "React.js", icon: <TbBrandReact /> },
               ]}
-              text="Creador de listas de tareas, se puede añadir, marcar y eliminar las tareas dadas, realizado con Javascript y el diseño por CSS3. "
-              sourceLink="https://todolist-design-app.netlify.app//"
-              appLink="https://github.com/JazminBoada/todo-list"
+              text="Creador de notas desde la funcionalidad CRUD, realizado con react.js y Next.js"
+              sourceLink="https://github.com/JazminBoada/nextjs-todo-app"
+              appLink="https://note-app1-nextjs.vercel.app/"
             />
             <ProjectsCards
               image={img4}
